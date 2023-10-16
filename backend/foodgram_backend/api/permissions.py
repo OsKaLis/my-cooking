2a85a812d98e4ce4a0939ecf94e4a0a5d511925c
Юра Юшко <oskaLis@yandex.ru>
2023-10-16 22:08:44 +0300

    2-stage v24

--- backend/foodgram_backend/api/permissions.py
+++ backend/foodgram_backend/api/permissions.py
@@ -12,7 +12,6 @@ class ProfileReadOnly(permissions.IsAuthenticated):
 
 
 class AuthenticatedOrReadOnly(permissions.BasePermission):
-
     def has_object_permission(self, request, view, obj):
         if view.action in ['create', 'update', 'destroy']:
             return request.user.is_authenticated
